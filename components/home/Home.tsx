ec162588ca0342233481900daab906114e53da17
fasluolassery <fasluolassery@gmail.com>
2026-07-03 16:35:47 +0530

    refactor(home): remove pre-existing layout columns
    Clean up the Home component layout by removing StatsBar, FilterSidebar, and JobList references.

--- components/home/Home.tsx
+++ components/home/Home.tsx
@@ -2,11 +2,6 @@
 
 import { useState } from 'react';
 import { LocationSelector } from '@/components/shared/LocationSelector';
-import { FilterSidebar } from '@/components/shared/FilterSidebar';
-import { StatsBar } from '@/components/home/StatsBar';
-import { JobList } from '@/components/home/JobList';
-import { mockJobs } from '@/lib/data';
-import { FilterOptions } from '@/types';
 import { locationCookie } from '@/utils/location-cookie';
 import { Hero } from './Hero';
 
@@ -16,12 +11,6 @@ interface HomeProps {
 
 export default function Home({ initialLocation }: HomeProps) {
   const [selectedLocation, setSelectedLocation] = useState<string | null>(initialLocation);
-  const [searchQuery] = useState('');
-
-  const jobs = mockJobs.map((job) => ({
-    ...job,
-    location: selectedLocation || job.location,
-  }));
 
   const handleChangeLocation = () => {
     locationCookie.remove();
@@ -36,17 +25,6 @@ export default function Home({ initialLocation }: HomeProps) {
     <main className="flex-grow bg-linear-to-br from-white via-[#FAFAFA] to-white">
       {/* Landing Page Hero Section */}
       <Hero selectedLocation={selectedLocation} onChangeLocation={handleChangeLocation} />
-
-      <div className="max-w-[1440px] mx-auto px-4 sm:px-8 lg:px-12 py-10">
-        <StatsBar />
-
-        <div className="grid grid-cols-1 lg:grid-cols-4 gap-8 mt-10">
-          <div className="lg:col-span-1">
-            <FilterSidebar onFilterChange={(filters: FilterOptions) => console.log(filters)} />
-          </div>
-          <JobList jobs={jobs} searchQuery={searchQuery} />
-        </div>
-      </div>
     </main>
   );
 }
